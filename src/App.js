6df86235394c36388faf0d0962e513e4e0f7524d
Nontakon <nontakon.charoen@gmail.com>
2020-06-25 23:44:57 +0700

    Add storeprovider,route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import './App.css';
 import Home from './components/Home'
+import Details from './components/Details'
+import Heros from './components/Heros'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { StoreContext,useStoreReducer } from "./store/storeprovider";
+
+
 function App() {
+  const [state, dispatch] = useStoreReducer()
   return (
     
-    <div className="App">
-      <header className="App-header">
-        <Home/>
-      </header>
-    </div>
+    <StoreContext.Provider value={{state,dispatch}}>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/Detail">
+              <Details/>
+            </Route>
+            <Route path="/Hero">
+              <Heros/>
+            </Route>
+            <Route path="/"> 
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </StoreContext.Provider>
   );
 }
 
